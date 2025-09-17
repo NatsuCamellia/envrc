9d6b7a605151cc0e79aaf2138e93ca01dc103639
NatsuCamellia <natsucamellia@gmail.com>
2025-09-17 11:19:50 +0800

    Remove .zshrc and add p10k config

--- install.sh
+++ install.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 # Define the paths to the original files
-zshrc_path=".zshrc"
 vimrc_path=".vimrc"
+p10k_path=".p10k.zsh"
 
 echo "Backing up existing files"
 # Create backups of existing files (if any)
-if [ -f "$HOME/.zshrc" ]; then
-    rm -f "$HOME/.zshrc.backup"
-    mv -f "$HOME/.zshrc" "$HOME/.zshrc.backup"
-    echo "Backed up $HOME/.zshrc to $HOME/.zshrc.backup"
+if [ -f "$HOME/$vimrc_path" ]; then
+    mv -f "$HOME/$vimrc_path" "$HOME/$vimrc_path.backup"
+    echo "Backed up $HOME/$vimrc_path to $HOME/$vimrc_path.backup"
 fi
 
-if [ -f "$HOME/.vimrc" ]; then
-    rm -f "$HOME/.vimrc.backup"
-    mv -f "$HOME/.vimrc" "$HOME/.vimrc.backup"
-    echo "Backed up $HOME/.vimrc to $HOME/.vimrc.backup"
+if [ -f "$HOME/$p10k_path" ]; then
+    mv -f "$HOME/$p10k_path" "$HOME/$p10k_path.backup"
+    echo "Backed up $HOME/$p10k_path to $HOME/$p10k_path.backup"
 fi
 
 # Create symbolic links to the original files
 echo "Creating symbolic links"
-ln "$zshrc_path" "$HOME/.zshrc"
-ln "$vimrc_path" "$HOME/.vimrc"
+ln -f -s "$(pwd)/$vimrc_path" "$HOME/$vimrc_path"
+ln -f -s "$(pwd)/$p10k_path" "$HOME/$p10k_path"
 
 echo "Installation complete"
