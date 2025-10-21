87ad951ae3f8617336a32058d6987e3e6ac68590
NatsuCamellia <natsucamellia@gmail.com>
2025-10-21 19:57:17 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -6,24 +6,24 @@ if [ ! -d "$HOME/powerlevel10k" ]; then
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 fi
 
-# Define the paths to the original files
-nvim_path=".config/nvim"
-p10k_path=".p10k.zsh"
-
-if [ -f "$HOME/$p10k_path" ]; then
-	mv -f "$HOME/$p10k_path" "$HOME/$p10k_path.backup"
-	echo "Backed up $HOME/$p10k_path to $HOME/$p10k_path.backup"
+# Install Tmux Plugin Manager if not found
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-if [ -d "$HOME/$nvim_path" ]; then
-	mv -f "$HOME/$nvim_path" "$HOME/$nvim_path.backup"
-	echo "Backed up $HOME/$nvim_path to $HOME/$nvim_path.backup"
-fi
+install_dotfile() {
+	local filename=$1
+	if [ -f "$HOME/$filename" ]; then
+		mv -f "$HOME/$filename" "$HOME/$filename.backup"
+		echo "Backed up $HOME/$filename to $HOME/$filename.backup"
+	fi
+	ln -f -s "$SCRIPT_DIR/$filename" "$HOME/$filename"
+	echo "Created symbolic link $SCRIPT_DIR/$filename -> $HOME/$filename"
+}
 
-# Create symbolic links to the original files
-echo "Creating symbolic links"
-ln -f -s "$SCRIPT_DIR/$p10k_path" "$HOME/$p10k_path"
-ln -f -s "$SCRIPT_DIR/$nvim_path" "$HOME/$nvim_path"
+install_dotfile ".config/nvim"
+install_dotfile ".p10k.zsh"
+install_dotfile ".tmux.conf"
 
-echo "Installation complete"
+echo "Installation completed"
 
